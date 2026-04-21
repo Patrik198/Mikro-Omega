cfefe63b619892538e14070d18aae56aaf500e0e
hrbek2 <hrbek2@spsejecna.cz>
2026-04-21 08:36:17 +0200

    Vytvoreni dalsiho okna po stisku tlacitka a pridani tlacitka

--- src/TitleScreen.java
+++ src/TitleScreen.java
@@ -4,10 +4,16 @@ import java.awt.*;
 public class TitleScreen extends JFrame {
 
     public TitleScreen(){
-        super("Title screen");
+        super("Menu");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setBackground(Color.PINK);
         setLocationRelativeTo(null);
         setExtendedState(MAXIMIZED_BOTH);
+        setLayout(new BorderLayout());
+        JButton b1 = new JButton("Start");
+        add(b1, BorderLayout.PAGE_START);
+        b1.setFont(new Font("Serif", Font.BOLD, 45));
+        b1.setBounds(75, 150, 75, 150);
+        b1.setFocusPainted(false);
     }
 }
